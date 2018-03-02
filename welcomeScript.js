7f537fbdd666399ebf38913aaf85333bf0962d93
Renzo Schindeler <renzo@schindeler.nl>
2018-03-02 14:51:58 +0100

    wanhopige halfwerkendecode

--- welcomeScript.js
+++ welcomeScript.js
@@ -2,4 +2,10 @@ function triggerWelcome() {
     alert("Page is loaded");
     document.getElementById("welcomeText").innerHTML = "Welcome to our website!";
 }
-
+/*
+document.addEventListener('DOMContentLoaded', function() {
+    alert("HASDHASD");
+    triggerWelcome();
+}, false);
+alert("dfsfdsdsfsd");
+*/
