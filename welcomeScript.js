33a39f747f63b23633bb92c1c46a60ef73def4db
Renzzauw <renzo@schindeler.nl>
2018-03-05 15:33:07 +0100

    Script op homepage werkt nu als hij laad

--- welcomeScript.js
+++ welcomeScript.js
@@ -1,11 +1,7 @@
-function triggerWelcome() {
-    alert("Page is loaded");
-    document.getElementById("welcomeText").innerHTML = "Welcome to our website!";
-}
-/*
-document.addEventListener('DOMContentLoaded', function() {
-    alert("HASDHASD");
-    triggerWelcome();
-}, false);
-alert("dfsfdsdsfsd");
-*/
+window.onload = function () {
+    // Welcome the visitor with an alert
+    alert("Hello and welcome to our website!");
+    // Change the default page title with a welcome message
+    document.getElementById("welcomeText").innerHTML = "Welcome to our Website!"; 
+};
+
