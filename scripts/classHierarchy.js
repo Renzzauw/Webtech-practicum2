7c39b25e39e54937a954d52f04469a900d76aef9
Renzzauw <renzo@schindeler.nl>
2018-03-09 22:38:21 +0100

    Script verwijdert nu "next article" link bij laatste article

--- scripts/classHierarchy.js
+++ scripts/classHierarchy.js
@@ -1,5 +1,4 @@
 // global variables 
-//articlelist;
 articlecounter = 0;
 
 // create an empty nav menu to fill after filling the page with articles
@@ -9,13 +8,13 @@ $( "main" ).append('<nav id="nav-menu-kb"></nav>');
 class SingleParagraphArticle {
     constructor(title, text) {
         this.title = title;
-        this.text = text;
-        //articlelist.push(this.title);    
+        this.text = text;   
         articlecounter++;
     }
     // function to turn this article instance into HTML
     turnIntoHTML() {
         $( "main" ).append('<article class="kb-articles" id="art-'+articlecounter+'"><h2>'+this.title+'</h2><p>'+this.text+'</p><p><a href="#art-'+(articlecounter+1)+'">Next Article</a></p></article>');
+        // add this article to the nav menu on top
         $( "#nav-menu-kb" ).append('<a href="#art-'+articlecounter+'">'+this.title+'</a>');        
     }
 }
@@ -43,17 +42,12 @@ function createArticle (articleKind, title, content){
 // creation of various articles
 createArticle(SingleParagraphArticle, "Article1", ["This is absolutely not a test."]);
 createArticle(SingleParagraphArticle, "Article2", ["This is absolutely not a test too."]);
-
+createArticle(SingleParagraphArticle, "Article3", ["This is absolutely not a test."]);
+createArticle(SingleParagraphArticle, "Article4", ["This is absolutely not a test too."]);
 //createArticle(DoubleParagraphArticle, "Article2", ["This is a nice article.", "Did I mention this one has 2 paragraphs?"]);
 
+// remove last link to next article (sorry, didn't have time to make some sort of checker in the HTML generator ;-(  )
+links = $( "a" );
+linkslength = links.length;
+links[linkslength-1].remove("a");
 
-
-
-/*
-article1 = new SingleParagraphArticle("Article1", "This is absolutely not a test.");
-article1.turnIntoHTML();
-article2 = new SingleParagraphArticle("Article2", "This is absolutely not a test too.");
-article2.turnIntoHTML();
-article3 = new DoubleParagraphArticle("Article3", "This is a nice article.", "Did I mention this one has 2 paragraphs?");  
-article3.turnIntoHTML();
-*/
