ac9bd12b66e87601eab417094d587667f9a43c23
rickbou11 <rick.boumans98@gmail.com>
2018-03-10 20:01:52 +0100

    separated scripts (again)

--- scripts/boldScript.js
+++ scripts/boldScript.js
@@ -1,3 +1,42 @@
-function myFunction() {
-    document.getElementById("size").style.fontSize = larger;
+var contextMenu = document.getElementById('contextMenu');
+
+function hey(){
+    $("#contextMenu").toggle();
+}
+
+function clicky(){
+    alert(clicked);
+}
+
+var setbold = document.getElementById('bold');
+setbold.onclick = boldFunc;
+
+var setunbold = document.getElementById('unbold');
+setunbold.onclick = unboldFunc;
+
+var setred = document.getElementById('red');
+setred.onclick = redFunc;
+var setgreen = document.getElementById('green');
+setgreen.onclick = greenFunc;
+var setblue = document.getElementById('blue');
+setblue.onclick = blueFunc;
+
+function boldFunc() {
+    document.getElementById('modtext').style.fontWeight = "bold";
+}
+
+function unboldFunc() {
+    document.getElementById('modtext').style.fontWeight = "normal";
+}
+
+function redFunc(){
+    document.getElementById('modtext').style.color = "red";
+}
+
+function greenFunc(){
+    document.getElementById('modtext').style.color = "green";
+}
+
+function blueFunc(){
+    document.getElementById('modtext').style.color = "blue";
 }
