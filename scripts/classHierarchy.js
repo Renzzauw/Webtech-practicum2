0bbc27e4e5ec3592bd5468730dc619e971a280bb
Renzzauw <renzo@schindeler.nl>
2018-03-13 19:57:42 +0100

    3e class nu toegevoegd

--- scripts/classHierarchy.js
+++ scripts/classHierarchy.js
@@ -33,15 +33,30 @@ class DoubleParagraphArticle extends SingleParagraphArticle {
     }
 }
 
+// class that extends the previous class with an extra paragraph
+class TripleParagraphArticle extends DoubleParagraphArticle {
+    constructor(title, text, text2, text3) {
+        super(title, text, text2);
+        this.text3 = text3;
+    }
+    // function to turn this article instance into HTML
+    turnIntoHTML() {
+        $( "main" ).append('<article class="kb-articles" id="art-'+articleCounter+'"><h2>'+this.title+'</h2><p>'+this.text+'</p><p>'+this.text2+'</p><p>'+this.text3+'</p><p><a href="#art-'+(articleCounter+1)+'">Next Article</a></p></article>');
+        // add this article to the nav menu on top
+        $( "#nav-menu-kb" ).append('<a href="#art-'+articleCounter+'">'+this.title+'</a>'); 
+    }
+}
+
 // function that creates the article
 function createArticle (articleKind, title, content){
-    article = new articleKind(title, content[0], content[1]);
+    article = new articleKind(title, content[0], content[1], content[2]);
     article.turnIntoHTML();
 }
 
 // creation of various articles
 createArticle(SingleParagraphArticle, "Kinds Of Wood", ["Hello! We sell many kinds of wood, read further for various kinds of wood."]);
 createArticle(SingleParagraphArticle, "Acacia", ["This is a very nice tree."]);
+createArticle(TripleParagraphArticle, "Article with 3 P's", ["This is a nice article.", "Did I mention this one has 3 paragraphs?", "Look, here's the 3th one!"]);
 createArticle(SingleParagraphArticle, "Alder", ["This is a very nice tree."]);
 createArticle(SingleParagraphArticle, "Ash", ["This is a very nice tree."]);
 createArticle(SingleParagraphArticle, "Bengi", ["This is a very nice tree."]);
